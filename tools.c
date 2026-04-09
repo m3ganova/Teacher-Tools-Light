e96b92eb51ac1779b9bd66c2072bccbb99fe6b73
m3ganova <notbegula@protonmail.com>
2026-04-09 20:37:37 +0300

    Backup

--- tools.c
+++ tools.c
@@ -13,6 +13,27 @@ int *stat = &status;
 
 void enterCommand();
 
+void calculator(){
+    
+    // Variable init
+    char operand = 'm';
+    float x = 0;
+    float y = 0;
+    char *op = &operand;
+    float *a = &x;
+    float *b = &y;
+    
+printf("Enter what you want to calculate. Format is: operand operator operand and add a space or enter in between.\n");
+    
+    scanf("%f%c%f", a,op,b);
+        if (strcmp(op, "+") == 0){
+            printf("%.3f + %.3f = %.2f\n", x, y, x + y);
+            status = 1;
+        } else if (strcmp(op, "+") == 7) {
+            printf("Got lazy :P\n");
+        }
+}
+
 void markingTests(){
     float totalMarks;
     float missedMarks;
@@ -29,14 +50,14 @@ void markingTests(){
     scanf("%f", mM);
     scoredMarks = (((totalMarks - missedMarks)/totalMarks)* 100);
     printf("The student has scored %.0f out of %.0f and has scored %.2f percent.\n", totalMarks - missedMarks, totalMarks, scoredMarks);
-    enterCommand();
+    status = 1;
 
 }
 
 
 void allCommands(){
-    printf("\nAll Commands:  \n\n tt--v / --version: View Teacher Tools version.\n tt--h / --help: View all commands.\n tt--x / --exit: Close the program.\n\nTools:\n\n --gg / grade :  Grade Generator - Outputs a letter depending on the students percentage grade. (NOT IMPLEMENTED YET!)\n --mk / mark : Marking Tests - Marks a test and outputs the marks earned and percentage grade.  \n --calc / calculator : Calculator - Calculates your equation.  \n");
-    enterCommand();
+    printf("\nAll Commands:  \n\n --v / --version: View Teacher Tools version.\n --h / help: View all commands.\n --x / quit: Close the program.\n\nTools:\n\n --gg / grade :  Grade Generator - Outputs a letter depending on the students percentage grade. (NOT IMPLEMENTED YET!)\n --mk / mark : Marking Tests - Marks a test and outputs the marks earned and percentage grade.  \n --calc / calculator : Calculator - Calculates your equation.  \n");
+    status = 1;
 }
 
 void enterCommand()
@@ -46,12 +67,23 @@ void enterCommand()
     status = 1;
     printf("Enter a command: ");
     scanf("%s",com);
-    if (strcmp(input, "--mk") == 0 || strcmp(input, "mark") == 0) {
+    if (strcmp(input, "--mk") == 0 || strcmp(input, "mark") == 0) { // The command for markingTests()
         status = 2;
-    } else if (strcmp(input, "--help") == 0 || strcmp(input, "help") == 0){
+    } else if (strcmp(input, "--calc") == 0 || strcmp(input, "calculator") == 0){
         status = 3;
-    } else {
-        printf("Print a valid command.\n");
+    }else if (strcmp(input, "--h") == 0 || strcmp(input, "help") == 0){ // The command for allCommands()
+        status = 4;
+    } 
+      else if ((strcmp(input, "--v") == 0) || strcmp(input, "version") == 0) { // Prints the version. It is just too
+                                                                              // simple to have its own status code.
+        printf("The Teacher Tools version is %.2f.\n", version);
+        status = 1;
+    }  
+    else if ( strcmp(input, "--x") == 0 || strcmp(input, "quit") == 0 ) { // The command to exit the program
+        status = 0;
+    }
+    else {
+        printf("Enter a valid command.\n");
         status = 1;
     }
 }
@@ -65,8 +97,13 @@ int main(){
         } else if (status == 2){
             markingTests();
         } else if (status == 3){
+            calculator();
+        }
+            else if (status == 4){
             allCommands();
         } else if (status == 0){
+            printf("The program will terminate shortly.\n");
+            exit(0);
             break;
         }
         else {
